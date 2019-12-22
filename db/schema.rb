42a54001df510f3d190ae082dbfd266099b75556
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-12-22 15:39:36 +0200

    fix bug with lectures

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,27 @@ ActiveRecord::Schema.define(version: 2019_12_19_171100) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "students" because of following StandardError
-#   Unknown type 'int2vector' for column 'phone'
+  create_table "students", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "surname"
+    t.string "fathername"
+    t.integer "phone"
+    t.string "groupname"
+    t.string "student"
+    t.string "teacher"
+    t.string "curator"
+    t.string "position"
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["groupname"], name: "index_students_on_groupname", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  end
 
   create_table "subject_themes", force: :cascade do |t|
     t.string "theme_name"
